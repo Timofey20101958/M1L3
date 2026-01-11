666178d8c3179fb74b5015ba7eff381fe6dad875
Timofey20101958 <tzologin@internet.ru>
2026-01-11 17:13:24 +0700

    Изменение нового хэндлера

--- bot.py
+++ bot.py
@@ -23,6 +23,24 @@ def ban_user(message):
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
 
+iventparticipant = ()
+question = ()
+answer_admin = ()
+answer_user = ()
+@bot.message_handler(commands=['ivent'])
+def ivent(message):
+    iventparticipant(input('Выбирите 1- чтобы ответить на вопрос или 2- чтобы добавить вопрос'))
+    if iventparticipant == 1:
+        print(question)
+        print('Выбирите вариант ответа')
+        answer_user = input(answer_admin)
+    if iventparticipant == 2:
+        print('Введите свой вопрос')
+        question= input('пример: Какое сегодня число?')
+        print('Введите варианты ответа')
+        answer_admin = input('пример: 1- 20.11.2025 2- 01.01.2026 3- 11.01.2026')
+        print('Вы создали вопрос:', question, 'Отыетами на каторый является', answer_admin)
+
 @bot.message_handler(content_types=['new_chat_members'])
 def make_some(message):
     bot.send_message(message.chat.id, 'Я добавил нового пользователя!')
